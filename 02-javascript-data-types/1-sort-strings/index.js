520980a1a6f220e64d274b1a906149bbc4c45c10
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-09-20 20:07:08 +0400

    Added solution for 02/1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const locales = ['ru', 'en'];
+  const sortedAsc = arr.toSorted((a, b) => {
+    return a.localeCompare(b, locales, {
+      caseFirst: 'upper',
+    });
+  });
 
+  switch (param) {
+  case 'asc':
+    return sortedAsc;
+  case 'desc':
+    return sortedAsc.reverse();
+  default:
+    throw new Error(`Order should be 'asc' or 'desc', received ${param}`);
+  }
 }
