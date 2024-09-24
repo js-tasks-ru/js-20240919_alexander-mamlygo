83a4798719e20dfe8ed73d1d71d21648352883e1
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-09-24 17:50:57 +0400

    Added solution to 03/1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,19 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+function createGetter(path) {
+  const pathElements = path.split('.');
 
+  return function (obj) {
+    let result = obj;
+
+    for (let pathElement of pathElements) {
+      if (typeof result !== 'object' || result === null || !result.hasOwnProperty(pathElement)) {
+        return undefined;
+      }
+      result = result[pathElement];
+    }
+
+    return result;
+  };
 }
