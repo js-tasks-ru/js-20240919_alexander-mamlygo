a8b595531b01c5d5938ad9848535b8899ba26876
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-10-06 13:21:52 +0400

    Added 05/1-notification solution

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
 export default class NotificationMessage {
+  static lastShownNotification;
+  element;
+  timerId;
 
+  constructor(message, props = {}) {
+    this.message = message;
+
+    const {
+      duration = 20,
+      type
+    } = props;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.createElement();
+  }
+
+  show(container = document.body) {
+    if (NotificationMessage.lastShownNotification) {
+      NotificationMessage.lastShownNotification.remove();
+    }
+
+    this.timerId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+    container.append(this.element);
+    NotificationMessage.lastShownNotification = this;
+  }
+
+  remove() {
+    this.destroy();
+  }
+
+  destroy() {
+    this.element.remove();
+    NotificationMessage.lastShownNotification = null;
+    clearTimeout(this.timerId);
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTemplate();
+
+    if (this.type === 'success') {
+      element.firstElementChild.classList.add('success');
+    } else if (this.type === 'error') {
+      element.firstElementChild.classList.add('error');
+    }
+
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return `
+       <div class="notification" style="--value:${this.duration}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
 }
